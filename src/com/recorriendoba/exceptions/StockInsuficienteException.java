f22b6ec4508adb63049ade160f491768ed7c0171
Ana <anita_llanes@hotmail.com>
2026-05-15 03:55:50 -0300

    Actualizo el proyecto con las correcciones del README y exceptions

---
+++ src/com/recorriendoba/exceptions/StockInsuficienteException.java
@@ -0,0 +1,7 @@
+package com.recorriendoba.exceptions;
+
+public class StockInsuficienteException extends Exception {
+    public StockInsuficienteException(String mensaje) {
+        super(mensaje);
+    }
+}
